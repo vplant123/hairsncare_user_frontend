f98e495acd05753d51a2afbf6e486b74f9a830f6
kuldeep kumar <kuldeep.kumar@Fonixtech.io>
2025-07-12 19:03:21 +0530

    update content file

--- src/app/ContentApi.js
+++ src/app/ContentApi.js
@@ -1,11 +1,27 @@
+// // import axios from 'axios';
+// import BASE_URL from '../Config';
+
+// export const getUtilityData = async () => {
+//   // const response = await axios.post(`${BASE_URL}/utility/getContent`);
+
+//   const response = await fetch(`${BASE_URL}/utility/getContent`, {
+//     method: 'POST'
+//   });
+//   return response.data;
+// };
+
+
+
+import axios from 'axios';
 // import axios from 'axios';
 import BASE_URL from '../Config';
 
 export const getUtilityData = async () => {
+  const response = await axios.post(`${BASE_URL}/utility/getContent`);
   // const response = await axios.post(`${BASE_URL}/utility/getContent`);
 
-  const response = await fetch(`${BASE_URL}/utility/getContent`, {
-    method: 'POST'
-  });
+  // const response = await fetch(`${BASE_URL}/utility/getContent`, {
+  //   method: 'POST'
+  // });
   return response.data;
 };

be8cdfcba3849c64e21ec33564fc0566b001332f
Gaurav Singh Solanki <gaurav.singh@fonixtech.io>
2025-08-21 11:20:19 +0530

    Update Product.js

--- src/features/product-list/Product.js
+++ src/features/product-list/Product.js
@@ -1,52 +1,60 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import './Product.css';
-import { useNavigate } from 'react-router-dom';
-import BASE_URL from '../../Config';
-import { useMediaQuery } from '@mui/material';
-import useDivInView, { RIGHT_VARIANTS, TRANSITION } from '../../hooks/useDivInView';
+import React, { useEffect, useMemo, useState } from "react";
+import "./Product.css";
+import { useNavigate } from "react-router-dom";
+import BASE_URL from "../../Config";
+import { useMediaQuery } from "@mui/material";
+import useDivInView, {
+  RIGHT_VARIANTS,
+  TRANSITION,
+} from "../../hooks/useDivInView";
 import { motion } from "framer-motion";
-import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
-import { getCartItems } from '../products/CartSlice';
-import useNavigateParams from '../../utils/hookUseNavigateParam';
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { getCartItems } from "../products/CartSlice";
+import useNavigateParams from "../../utils/hookUseNavigateParam";
 
 let data = [
   {
     src: "/assets/img/product/1.png",
     name: "Hair Vitamin with Biotin",
     rating: 4,
-    massage: "100% make a type specimen book. It has survived not only centuries,",
-    logo: "/assets/img/google-icon.webp"
+    massage:
+      "100% make a type specimen book. It has survived not only centuries,",
+    logo: "/assets/img/google-icon.webp",
   },
   {
     src: "/assets/img/product/2.png",
     name: "Gummies with Biotin",
     rating: 4,
-    massage: "100% make a type specimen book. It has survived not only centuries,",
-    logo: "/assets/img/google-icon.webp"
+    massage:
+      "100% make a type specimen book. It has survived not only centuries,",
+    logo: "/assets/img/google-icon.webp",
   },
   {
     src: "/assets/img/product/3.png",
     name: "Vitamin D",
-    massage: "100% make a type specimen book. It has survived not only centuries,",
+    massage:
+      "100% make a type specimen book. It has survived not only centuries,",
     rating: 4,
-    logo: "/assets/img/google-icon.webp"
+    logo: "/assets/img/google-icon.webp",
   },
   {
     src: "/assets/img/product/4.png",
     name: "Hair Shampoo",
     rating: 4,
-    massage: "100% make a type specimen book. It has survived not only centuries,",
-    logo: "/assets/img/google-icon.webp"
-  }
+    massage:
+      "100% make a type specimen book. It has survived not only centuries,",
+    logo: "/assets/img/google-icon.webp",
+  },
 ];
 
-const ProductCard = ({ product, index, cart,
-  setCart }) => {
-
+const ProductCard = ({ product, index, cart, setCart }) => {
   const navigate = useNavigateParams();
   const [ref, control] = useDivInView();
-  let disPercent = ((parseFloat(product?.discount || 0) / parseFloat(product?.price)) * 100)?.toFixed(0)
+  let disPercent = (
+    (parseFloat(product?.discount || 0) / parseFloat(product?.price)) *
+    100
+  )?.toFixed(0);
 
   let storedUserData = JSON.parse(localStorage?.getItem("User343"));
   const dispatch = useDispatch();
@@ -54,12 +62,11 @@ const ProductCard = ({ product, index, cart,
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
       /* you can also use 'auto' behaviour 
          in place of 'smooth' */
     });
   };
-
 
   const handleAddToCart = async () => {
     let data = {
@@ -67,13 +74,11 @@ const ProductCard = ({ product, index, cart,
       quantity: 1,
     };
     if (!storedUserData?.logedInUser) {
-
-      let c = cart
+      let c = cart;
       let f = c?.findIndex((w) => w?.item?._id == product?._id);
       if (f != -1) {
         c[f].quantity = c[f]?.quantity + 1;
-      }
-      else {
+      } else {
         c.push(data);
       }
       setCart(c);
@@ -99,7 +104,7 @@ const ProductCard = ({ product, index, cart,
       if (response.ok) {
         const result = await response.json();
         dispatch(getCartItems(storedUserData.logedInUser.user._id));
-        navigate("/cart")
+        navigate("/cart");
         console.log("review created successfully:", result);
         // navigate("/cart");
       } else {
@@ -117,16 +122,17 @@ const ProductCard = ({ product, index, cart,
       animate={control}
       initial="hidden"
       variants={RIGHT_VARIANTS}
-      transition={
-        {
-          ...TRANSITION,
-          delay: index * 0.2
-        }
-      }
-      style={{ cursor: 'pointer' }}
+      transition={{
+        ...TRANSITION,
+        delay: index * 0.2,
+      }}
+      style={{ cursor: "pointer" }}
       className="col-12 col-md-4 "
     >
-      <div className="product-card" style={{ height: '450px', justifyContent: 'space-between' }}>
+      <div
+        className="product-card"
+        style={{ height: "450px", justifyContent: "space-between" }}
+      >
         <div>
           {disPercent && disPercent != "0" ? (
             <div style={{ position: "absolute", zIndex: 1 }}>
@@ -139,55 +145,76 @@ const ProductCard = ({ product, index, cart,
           ) : (
             <></>
           )}{" "}
-          <img src={product.src?.[0]} alt={product.name} title={product.name} style={{ height: '200px' }} onClick={() => {
-            scrollToTop();
-            navigate('/product-detail/' + product?.metaSlug ?? product._id, { id: product?._id })
-          }
-          }
+          <img
+            src={product.src?.[0]}
+            alt={product.name}
+            title={product.name}
+            style={{ height: "200px" }}
+            onClick={() => {
+              scrollToTop();
+              navigate("/product-detail/" + product?.metaSlug ?? product._id, {
+                id: product?._id,
+              });
+            }}
           />
         </div>
         <div className="d-flex flex-column">
           <div
             style={{
-              textAlign: 'center',
-              fontSize: '1rem',
-              fontWeight: '500',
+              textAlign: "center",
+              fontSize: "1rem",
+              fontWeight: "500",
             }}
             onClick={() => {
               scrollToTop();
-              navigate('/product-detail/' + product?.metaSlug ?? product._id, { id: product?._id })
-            }
-            }
+              navigate("/product-detail/" + product?.metaSlug ?? product._id, {
+                id: product?._id,
+              });
+            }}
           >
             {product.name}
           </div>
           <div
             style={{
               display: "flex",
-              textAlign: 'center',
-              fontSize: '1rem',
-              fontWeight: '500',
+              textAlign: "center",
+              fontSize: "1rem",
+              fontWeight: "500",
               justifyContent: "center",
-              gap: "10px"
+              gap: "10px",
             }}
             onClick={() => {
               scrollToTop();
-              navigate('/product-detail/' + product?.metaSlug ?? product._id, { id: product?._id })
-            }
-            }
+              navigate("/product-detail/" + product?.metaSlug ?? product._id, {
+                id: product?._id,
+              });
+            }}
           >
-            ₹ {parseFloat(product?.price || 0) - parseFloat(product?.discount || 0)}
-            {product?.discount ? <div
-              className="product-price-des"
-              style={{
-                paddingLeft: "10px",
-                fontWeight: "600",
-              }}
-            >
-              ₹{parseFloat(product.price)}
-            </div> : <></>}
+            ₹{" "}
+            {parseFloat(product?.price || 0) -
+              parseFloat(product?.discount || 0)}
+            {product?.discount ? (
+              <div
+                className="product-price-des"
+                style={{
+                  paddingLeft: "10px",
+                  fontWeight: "600",
+                }}
+              >
+                ₹{parseFloat(product.price)}
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
-          <ul style={{ padding: 0, display: "flex", justifyContent: "center", margin: "0" }}>
+          <ul
+            style={{
+              padding: 0,
+              display: "flex",
+              justifyContent: "center",
+              margin: "0",
+            }}
+          >
             <div class="ltn__comment-item clearfix">
               <div class="ltn__commenter-comment">
                 <div class="product-ratting">
@@ -253,47 +280,27 @@ const ProductCard = ({ product, index, cart,
                     <ul className="horizontal-list">
                       <li>
                         <a href="#">
-                          <i
-                            class={
-                              "far fa-star star-review-1-inactive"
-                            }
-                          ></i>
+                          <i class={"far fa-star star-review-1-inactive"}></i>
                         </a>
                       </li>
                       <li>
                         <a href="#">
-                          <i
-                            class={
-                              "far fa-star star-review-1-inactive"
-                            }
-                          ></i>
+                          <i class={"far fa-star star-review-1-inactive"}></i>
                         </a>
                       </li>
                       <li>
                         <a href="#">
-                          <i
-                            class={
-                              "far fa-star star-review-1-inactive"
-                            }
-                          ></i>
+                          <i class={"far fa-star star-review-1-inactive"}></i>
                         </a>
                       </li>
                       <li>
                         <a href="#">
-                          <i
-                            class={
-                              "far fa-star star-review-1-inactive"
-                            }
-                          ></i>
+                          <i class={"far fa-star star-review-1-inactive"}></i>
                         </a>
                       </li>
                       <li>
                         <a href="#">
-                          <i
-                            class={
-                              "far fa-star star-review-1-inactive"
-                            }
-                          ></i>
+                          <i class={"far fa-star star-review-1-inactive"}></i>
                         </a>
                       </li>
                     </ul>
@@ -302,7 +309,10 @@ const ProductCard = ({ product, index, cart,
               </div>
             </div>
           </ul>
-          <div className="btn-container" style={{ margin: 0 }} onClick={() => handleAddToCart()}
+          <div
+            className="btn-container"
+            style={{ margin: 0 }}
+            onClick={() => handleAddToCart()}
           >
             <button className="btn primary">ADD TO CART</button>
           </div>
@@ -316,87 +326,95 @@ const ProductList = ({ products, cart, setCart }) => {
   return (
     <div className="col-12 row">
       {products?.map((item, index) => (
-        <ProductCard key={index} product={item} index={index} cart={cart}
-          setCart={setCart} />
+        <ProductCard
+          key={index}
+          product={item}
+          index={index}
+          cart={cart}
+          setCart={setCart}
+        />
       ))}
     </div>
   );
 };
 
-
-
 const Pagination = ({ length, cur, setCur }) => {
   return (
-    <ul className="slick-dots" role="tablist" style={{ marginTop: '30px' }}>
-      {
-        Array.from({ length }).map((e, ind) => {
-          return (
-            <li
-              key={ind}
-              className={cur === ind + 1 ? 'slick-active' : ''}
-              role="presentation"
-              aria-selected={cur === ind + 1}
-              onClick={() => setCur(ind + 1)}
-            >
-              <button type="button" data-role="none" role="button">
-                {ind + 1}
-              </button>
-            </li>
-          );
-        })}
+    <ul className="slick-dots" role="tablist" style={{ marginTop: "30px" }}>
+      {Array.from({ length }).map((e, ind) => {
+        return (
+          <li
+            key={ind}
+            className={cur === ind + 1 ? "slick-active" : ""}
+            role="presentation"
+            aria-selected={cur === ind + 1}
+            onClick={() => setCur(ind + 1)}
+          >
+            <button type="button" data-role="none" role="button">
+              {ind + 1}
+            </button>
+          </li>
+        );
+      })}
     </ul>
   );
 };
 
-
 function Product(props) {
   const navigate = useNavigate();
   const handleViewAll = () => {
-    navigate('/best-hair-care-products-hair-loss-scalp-health');
+    navigate("/best-hair-care-products-hair-loss-scalp-health");
   };
-  let { cart,
-    setCart } = props;
+  let { cart, setCart } = props;
 
   const [products, setProducts] = useState([]);
-  const [status, setStatus] = useState('idle');
+  const [status, setStatus] = useState("idle");
   const [cur, setCur] = useState(1);
   const [slide] = useState(3);
 
   useEffect(() => {
-    let timeout = setTimeout(
-      () =>
-        setCur((prevIndex) =>
-          prevIndex === Math.ceil(products?.length / slide)
-            ? 1
-            : prevIndex + 1
-        ),
-      5000
-    );
+    if (products?.length >= 5) {
+      let timeout = setTimeout(
+        () =>
+          setCur((prevIndex) =>
+            prevIndex === Math.ceil(products?.length / slide)
+              ? 1
+              : prevIndex + 1
+          ),
+        15000
+      );
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [cur]);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [cur, products]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      setStatus('loading');
+      setStatus("loading");
       try {
         const response = await fetch(`${BASE_URL}/admin/product?review=4`);
         const data = await response.json();
-        setProducts([...data.message, ...data.message, ...data.message, ...data.message]); // Adjust according to your API response structure
-        setStatus('idle');
+        console.log("Products fetched:", data);
+        setProducts([
+          ...data.message,
+          // ...data.message,
+          // ...data.message,
+          // ...data.message,
+        ]); // Adjust according to your API response structure
+        setStatus("idle");
       } catch (error) {
-        console.error('Error fetching products:', error);
-        setStatus('error');
+        console.error("Error fetching products:", error);
+        setStatus("error");
       }
     };
 
     fetchProducts();
   }, []);
 
-  const isLargeScreen = useMediaQuery('(min-width:1200px)');
-  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isLargeScreen = useMediaQuery("(min-width:1200px)");
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   // useEffect(() => {
   //   if (isLargeScreen) {
@@ -406,29 +424,39 @@ function Product(props) {
   //   }
   // }, [isMobile, isLargeScreen]);
 
-  const paginatedProducts = useMemo(() => products?.slice((cur - 1) * slide, cur * slide), [cur, products, slide])
-
+  const paginatedProducts = useMemo(
+    () => products?.slice((cur - 1) * slide, cur * slide),
+    [cur, products, slide]
+  );
 
   return (
     <div>
       <h2
         className="product-title animate__animated animate__fadeIn"
-        style={{ fontSize: '2rem', fontWeight: '700' }}
+        style={{ fontSize: "2rem", fontWeight: "700" }}
       >
         Our Products
       </h2>
       <div className="product-container container row">
-        <ProductList products={paginatedProducts} cart={cart} setCart={setCart} />
-        <Pagination length={Math.ceil(products?.length / slide)} cur={cur} setCur={setCur} />
+        <ProductList
+          products={paginatedProducts}
+          cart={cart}
+          setCart={setCart}
+        />
+        <Pagination
+          length={Math.ceil(products?.length / slide)}
+          cur={cur}
+          setCur={setCur}
+        />
       </div>
       <div
         className="view-all-product animate__animated animate__fadeInDown"
-        style={{ animationDelay: '0.2s' }}
+        style={{ animationDelay: "0.2s" }}
       >
         <button
           onClick={handleViewAll}
           className="btn primary"
-          style={{ background: '#4b555c', color: 'white' }}
+          style={{ background: "#4b555c", color: "white" }}
         >
           VIEW ALL PRODUCTS
         </button>
